eed288511578967a77daba09bae0bb26290cecb6
tatendabakozw <tatendabakozw@gmail.com>
2023-03-18 17:35:50 +0200

    fix: added nall categories section

--- src/components/home-sections/DesktopView.tsx
+++ src/components/home-sections/DesktopView.tsx
@@ -284,7 +284,7 @@ const DesktopView = (props: Props) => {
             </Carousel>
           </div>
         </div> */}
-        {/* <div className="col-span-4 bg-white p-4">
+        <div className="col-span-4 bg-white p-4">
           <p className="text-slate-900 font-semibold text-lg pb-4">
             Top Sellers in Books for you
           </p>
@@ -297,7 +297,7 @@ const DesktopView = (props: Props) => {
               ))}
             </Carousel>
           </div>
-        </div> */}
+        </div>
 
         {/* beauty pick */}
         <CategoryItem heading="New Arrivals in toys" picture={toy_arrivals} />
